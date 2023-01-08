6e596a67f118117c05e9f0c3e83dd386ab55a32f
StudiosYouBedrock <112906890+StudiosYouBedrock@users.noreply.github.com>
2023-01-07 23:37:25 -0300

    Add files via upload

--- index_default.js
+++ index_default.js
@@ -10,3 +10,21 @@ function menu() {
         document.getElementById("menue").style.display = "block";
     }
 }
+
+function modal() {
+    let etiquetaAudio = document.createElement("audio")
+    etiquetaAudio.setAttribute("src", "assets/sound/release.ogg")
+    etiquetaAudio.play()
+}
+
+function modal_hide() {
+    let etiquetaAudio = document.createElement("audio")
+    etiquetaAudio.setAttribute("src", "assets/sound/modal_hide.ogg")
+    etiquetaAudio.play()
+}
+
+function snes_pop() {
+    let etiquetaAudio = document.createElement("audio")
+    etiquetaAudio.setAttribute("src", "assets/sound/snes_pop.ogg")
+    etiquetaAudio.play()
+}
